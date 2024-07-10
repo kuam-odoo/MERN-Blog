53e88c9dcdbce7e514d1ae076e67575bf05ec96d
kuam-odoo <kuam@odoo.com>
2024-07-10 12:54:12 +0530

    created the header with responsive UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,20 @@ import About from "./pages/About"
 import Dashboard from "./pages/Dashboard"
 import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
+import Projects from "./pages/Projects"
+import Header from "./components/Header"
+
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/About" element={<About/>}/>
     <Route path="/Dashboard" element={<Dashboard/>}/>
     <Route path="/sign-in" element={<Signin/>}/>
     <Route path="/sign-up" element={<Signup/>}/>
+    <Route path="/projects" element={<Projects/>}/>
     </Routes>
     </BrowserRouter>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,16 @@
+const Flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    Flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    Flowbite.plugin(),
+  ],
 }
